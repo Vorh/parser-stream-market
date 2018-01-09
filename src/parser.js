fa1e4353f7b86cdad7eb73b7dc03d9901f2145a7
Vorh <sorcerer12@list.ru>
2018-01-10 02:14:52 +0300

    [ ADD ] working parse

--- src/parser.js
+++ src/parser.js
@@ -3,28 +3,20 @@ const HTMLParser = require('fast-html-parser');
 const sleep = require('thread-sleep');
 
 const APP_ID = 730;
-const ALL_COUNT = 500;
-// const ALL_COUNT = 10;
-const PAGE_SIZE = 10;
-let OFFSET = 0;
+const ALL_COUNT = 1000;
+const PAGE_SIZE = 50;
 
 let countPerSecond = 0;
 
 
+getItems(0);
 
-for (; OFFSET < ALL_COUNT;) {
+function getItems(offset) {
 
-    let url = 'http://steamcommunity.com/market/search/render?appid=' + APP_ID + '&query=&start='+OFFSET+'&count=' + PAGE_SIZE;
-    console.log('URL: ' + url);
+    let url = 'http://steamcommunity.com/market/search/render?appid=' + APP_ID + '&query=&start=' + offset + '&count=' + PAGE_SIZE;
 
-    OFFSET = OFFSET + PAGE_SIZE;
-
-    if (countPerSecond===1){
-        countPerSecond=0;
-        sleep(1500);
-    }
-
-    countPerSecond++;
+    console.log(url);
+    sleep(10000);
 
     request(url, {json: true, style: false, script: false}, (err, res, body) => {
 
@@ -38,23 +30,17 @@ for (; OFFSET < ALL_COUNT;) {
 
         for (; countResultItem < PAGE_SIZE; countResultItem++) {
             let s = '#result_' + countResultItem + '_name';
-            try {
                 console.log(root.querySelector('#result_' + countResultItem + '_name.market_listing_item_name').childNodes[0].rawText);
-            } catch (e) {
-                console.log(e)
-            }
         }
 
+        offset = offset + PAGE_SIZE;
 
+        if (offset > ALL_COUNT) {
+            return;
+        }
+        getItems(offset);
     });
-
-
 }
-
-
-
-
-
 
 
 
