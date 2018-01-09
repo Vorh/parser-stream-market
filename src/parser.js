2102940c808a2377dcb11f63bb5c5672b2c5167a
Vorh <sorcerer12@list.ru>
2018-01-10 01:50:40 +0300

    [ ADD ] parse steam

--- src/parser.js
+++ src/parser.js
@@ -1,10 +1,15 @@
 const request = require('request');
 const HTMLParser = require('fast-html-parser');
+const sleep = require('thread-sleep');
 
 const APP_ID = 730;
-const ALL_COUNT = 10064;
-const PAGE_SIZE = 99;
+const ALL_COUNT = 500;
+// const ALL_COUNT = 10;
+const PAGE_SIZE = 10;
 let OFFSET = 0;
+
+let countPerSecond = 0;
+
 
 
 for (; OFFSET < ALL_COUNT;) {
@@ -13,6 +18,13 @@ for (; OFFSET < ALL_COUNT;) {
     console.log('URL: ' + url);
 
     OFFSET = OFFSET + PAGE_SIZE;
+
+    if (countPerSecond===1){
+        countPerSecond=0;
+        sleep(1500);
+    }
+
+    countPerSecond++;
 
     request(url, {json: true, style: false, script: false}, (err, res, body) => {
 
@@ -25,7 +37,7 @@ for (; OFFSET < ALL_COUNT;) {
 
 
         for (; countResultItem < PAGE_SIZE; countResultItem++) {
-            let s = '#result_' + countResultItem + 'name.market_listing_item_name';
+            let s = '#result_' + countResultItem + '_name';
             try {
                 console.log(root.querySelector('#result_' + countResultItem + '_name.market_listing_item_name').childNodes[0].rawText);
             } catch (e) {
@@ -33,7 +45,10 @@ for (; OFFSET < ALL_COUNT;) {
             }
         }
 
+
     });
+
+
 }
 
 
